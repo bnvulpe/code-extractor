207dc6c1d113a97af5f36ebf3741c86773fd470e
VIRO <116597344+VIR06@users.noreply.github.com>
2024-01-12 11:09:48 +0100

    Add files via upload

--- foto.py
+++ foto.py
@@ -46,19 +46,30 @@ def ocr(image, tesseract_path):
 
     pytesseract.pytesseract.tesseract_cmd = tesseract_path
 
-    config = ('-l eng --oem 3 --psm 6')
+    config = ('-l eng --oem 3 --psm 3')
     data = pd.DataFrame(pytesseract.image_to_data(final, config= config, output_type=pytesseract.Output.DICT))
 
-    lines = data[data['text'] != ''].line_num.unique()
-    tabs = data[data['text'] != ''].groupby('line_num').first().reset_index()['left'].values
+    data = data[data.text != '']
+    
+    final_list = []
+    for block in data.block_num.unique():
+        block_data = data[data.block_num == block]
+        for par in block_data.par_num.unique():
+            par_data = block_data[block_data.par_num == par]
+            for line in par_data.line_num.unique():
+                line_data = par_data[par_data.line_num == line]
+                ident = min(line_data.left)
+                text = ' '.join(line_data.text) + '\n'
+                final_list.append([text, ident])
 
-    min_tab = np.argmin(tabs)
-    tabs = (tabs / tabs[min_tab]).round(3).round(2).round(1).round(0).astype(int)
-    tabs = tabs - tabs[min_tab]
+    lefts = np.array([i[1] for i in final_list])
+    min_left = min(lefts)
+    tabs = (lefts / min_left).round(3).round(2).round(1).round(0).astype(int)
+    tabs -= 1
     tabs = substitute_with_index(tabs)
 
     txt = ''
-    for line, tab in zip(lines, tabs):
-        txt += '\t' * tab + ' ' + ' '.join(data[data['line_num'] == line].text.values) + '\n'
+    for i in range(len(final_list)):
+        txt += tabs[i]*'\t' + final_list[i][0]
 
     return txt
--- recorte.py
+++ recorte.py
@@ -18,20 +18,31 @@ def ocr(image, tesseract_path):
     thresholded = cv2.copyMakeBorder(thresholded,top =  int(0.05 * thresholded.shape[0]) ,bottom = int(0.05 * thresholded.shape[0]), left = int(0.05 * thresholded.shape[0]), right = int(0.05 * thresholded.shape[0]), borderType = cv2.BORDER_CONSTANT, value = int(thresholded[0][0]))
 
     pytesseract.pytesseract.tesseract_cmd = tesseract_path
-    config = ('-l eng --oem 3 --psm 6')
+    config = ('-l eng --oem 3 --psm 3')
     data = pd.DataFrame(pytesseract.image_to_data(thresholded, config=config, output_type=pytesseract.Output.DICT))
+    
+    data = data[data.text != '']
+    
+    final_list = []
+    for block in data.block_num.unique():
+        block_data = data[data.block_num == block]
+        for par in block_data.par_num.unique():
+            par_data = block_data[block_data.par_num == par]
+            for line in par_data.line_num.unique():
+                line_data = par_data[par_data.line_num == line]
+                ident = min(line_data.left)
+                text = ' '.join(line_data.text) + '\n'
+                final_list.append([text, ident])
 
-    lines = data[data['text'] != ''].line_num.unique()
-    tabs = data[data['text'] != ''].groupby('line_num').first().reset_index()['left'].values
-
-    min_tab = np.argmin(tabs)
-    tabs = (tabs / tabs[min_tab]).round(3).round(2).round(1).round(0).astype(int)
-    tabs -= tabs[min_tab]
+    lefts = np.array([i[1] for i in final_list])
+    min_left = min(lefts)
+    tabs = (lefts / min_left).round(3).round(2).round(1).round(0).astype(int)
+    tabs -= 1
     tabs = substitute_with_index(tabs)
 
     txt = ''
-    for line, tab in zip(lines, tabs):
-        txt += '\t' * tab + ' ' + ' '.join(data[data['line_num'] == line].text.values) + '\n'
+    for i in range(len(final_list)):
+        txt += tabs[i]*'\t' + final_list[i][0]
     
     return txt
 
